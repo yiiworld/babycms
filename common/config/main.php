850b9279c8294808ca1d47fd4b09cb7549f147a2
Funson Lee <funson86@gmail.com>
2014-09-22 15:11:45 +0800

    languages com

--- common/config/main.php
+++ common/config/main.php
@@ -6,5 +6,23 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'i18n' => [
+            'translations' => [
+                'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => "@yii/messages",
+                    'sourceLanguage' => 'zh',
+                    'fileMap' => [
+                        'yii'=>'yii.php',
+                        'authorization'=>'authorization.php',
+                        'app'=>'app.php',
+                    ]
+                ],
+                '*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'sourceLanguage' => 'zh',
+                ],
+            ]
+        ],
     ],
 ];
